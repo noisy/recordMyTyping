7d7463888e31c2dcaba00eb3e779c3e97e1a34cc
Krzysztof Szumny <krzysztof.szumny@spistresci.pl>
2015-06-29 23:50:04 +0200

    E01-00h01m30s

--- record.sh
+++ record.sh
@@ -2,4 +2,3 @@
 
 
 while :; do tag=`printf "E%02d-%02dh%02dm%02ds" $1 $(($SECONDS/3600)) $((($SECONDS%3600)/60)) $(($SECONDS%60))`; git commit -am "$tag"; git tag $tag ; sleep 1; done
-#to jest jakiś test
