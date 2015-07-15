f0179496f25118d31ac4f42f30436fe8a049edc1
Krzysztof Szumny <krzysztof.szumny@spistresci.pl>
2015-07-15 21:44:19 +0200

    some cleaning

--- record.sh
+++ record.sh
@@ -4,7 +4,8 @@
 # ./record.sh /full/path/to/repository EPISODE_NUMBER
 
 REPO_TO_RECORD=$1
-RECORDED_CLONED_REPO=$1/.git/timer_repo
+RECORDING_HOME_DIR=$1/.git/recording
+RECORDED_CLONED_REPO=$RECORDING_HOME_DIR/repo
 REPO_WITH_TAGS=$2
 
 # https://raw.githubusercontent.com/git/git/master/contrib/workdir/git-new-workdir
@@ -16,25 +17,33 @@ echo "sync {
     target='$RECORDED_CLONED_REPO',
     exclude = { '.git/' }
 }
-" > $REPO_TO_RECORD/.git/lsyncd.conf
+" > $RECORDING_HOME_DIR/lsyncd.conf
 
-lsyncd $REPO_TO_RECORD/.git/lsyncd.conf
+echo "#!/bin/bash
+while :; do clear; cat clone_link; sleep 1; done;
+" > $RECORDING_HOME_DIR/show_clone_link.sh && chmod +x $RECORDING_HOME_DIR/show_clone_link.sh
+
+
+lsyncd $RECORDING_HOME_DIR/lsyncd.conf
 
 
 cd $RECORDED_CLONED_REPO
 
 git checkout -b recording-`printf "E%02d" $3`
 
+### count down before recording
 secs=10
 while [ $secs -gt 0 ]; do echo -ne "Recording starts in $secs\033[0K\r"; sleep 1; : $((secs--)); done
 
+
+### recording
 while :; 
 do
   tag=`printf "E%02d-%02dh%02dm%02ds" $3 $(($SECONDS/3600)) $((($SECONDS%3600)/60)) $(($SECONDS%60))`;
   git add .
   git commit -m "$tag"; git tag $tag ;
 
-  echo -e "\n\tgit clone --branch $tag $REPO_WITH_TAGS\n" > $RECORDED_CLONED_REPO/../link_timer
+  echo -e "\n\tgit clone --branch $tag $REPO_WITH_TAGS\n" > $RECORDING_HOME_DIR/clone_link
 
   sleep 1; 
 done
